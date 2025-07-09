eccb99479a6bda13d91344f29eabbd3948796459
Danielle <danielle.shokrian@aol.com>
2025-07-09 15:48:23 -0400

    Functions Lab in JavaScript

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+function calculateTax(amount) {
+    return amount * (.10);
+}
+console.log(calculateTax(100))
+
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+console.log(convertToUpperCase("hello world"));
 
 
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}   
+
+console.log(findMaximum(10, 20));
 
 
+function isPalindrome(word) {
+    const cleaned = word.toLowerCase();
+    const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
+}   
+console.log(isPalindrome("Racecar"));
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * (discountPercentage / 100));
+}   
+console.log(calculateDiscountedPrice(100, 20)); 
+
+
